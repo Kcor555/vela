44ca21e0d13acb20e7774129c86d8be464d40a40
Kris Cochran <74980761+Kcor555@users.noreply.github.com>
2022-07-22 15:21:28 -0400

    functions current state

--- functions.php
+++ functions.php
@@ -16,22 +16,9 @@
  *
  */
 
-//
-//function vela_styles() {
-//	wp_enqueue_style(
-//		'vela-style',
-//		get_stylesheet_uri(),
-//		array(),
-//		wp_get_theme()->get( 'Version' )
-//	);
-//}
-//add_action( 'wp_enqueue_scripts', 'vela_styles' );
-
 if ( ! function_exists( 'vela_setup' ) )  :
 	function vela_setup() {
-
 		add_theme_support( 'wp-block-styles' );
-
 		add_editor_style(
 			[
 				'css/editor-style.css',
@@ -54,22 +41,21 @@ if ( ! function_exists( 'vela_setup' ) )  :
 		$theme_version  = wp_get_theme()->get( 'Version' );
 		$version_string = is_string( $theme_version ) ? $theme_version : 'false';
 
-		// Main stylesheet.
+		// Main stylesheet
 		wp_register_style(
 			'vela_styles',
-			get_stylesheet_directory_uri() . '/style.css' ,
+			get_stylesheet_directory_uri() . '/style.css',
 			[],
-		$version_string
+			$version_string
 		);
 		wp_enqueue_style( 'vela_styles' );
 
 		//Theme stylesheet
-
 		wp_register_style(
-	'vela-theme-style',
-	get_stylesheet_directory_uri() . '/assets/css/style.css',
-		[],
-		$version_string
+			'vela-theme-style',
+			get_stylesheet_directory_uri() . '/assets/css/style.css',
+			[],
+			$version_string
 		);
 
 		//Enqueue theme stylesheet
@@ -77,8 +63,6 @@ if ( ! function_exists( 'vela_setup' ) )  :
 
 		//Inline styles
 		wp_add_inline_style( 'vela_styles', 'vela_get_accent_colors()' );
-		wp_add_inline_style( 'vela-styles', vela_get_font_face_styles() );
-
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'vela_theme_style' );

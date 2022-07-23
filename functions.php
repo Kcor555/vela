7aca9b042a576fc19ce7aaea31411d7f41e2ea7f
Kris Cochran <74980761+Kcor555@users.noreply.github.com>
2022-07-23 01:51:05 -0400

    functions.php block styles

--- functions.php
+++ functions.php
@@ -8,62 +8,59 @@
  * @since   1.0.0
  */
 
+
+if ( ! function_exists( 'vela_setup' ) )  :
 /**
  * Theme defaults and support for WP features
  *
  * @return void
+ *
  * @since Vela 1.0
  *
  */
 
-if ( ! function_exists( 'vela_setup' ) )  :
 	function vela_setup() {
 		add_theme_support( 'wp-block-styles' );
-		add_editor_style(
-			[
-				'css/editor-style.css',
-				'vela-style'
-			]
-		);
+
+		add_editor_style( 'style.css', );
 	}
+
+	endif;
 
 	add_action( 'after_setup_theme', 'vela_setup' );
 
+	if ( ! function_exists( 'vela_theme_styles' ) )  :
 	/**
 	 * Enqueue styles.
 	 *
-	 * @return void
 	 * @since Vela 1.0
+	 *
+	 * @return void
 	 *
 	 */
 
-	function vela_theme_style() {
+	function vela_theme_styles() {
+		// Register theme stylesheet.
 		$theme_version  = wp_get_theme()->get( 'Version' );
+
 		$version_string = is_string( $theme_version ) ? $theme_version : 'false';
 
 		// Main stylesheet
 		wp_register_style(
-			'vela_styles',
+			'vela_theme_style',
 			get_stylesheet_directory_uri() . '/style.css',
 			[],
 			$version_string
 		);
-		wp_enqueue_style( 'vela_styles' );
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'vela_theme_style' );
 
-		//Theme stylesheet
-		wp_register_style(
-			'vela-theme-style',
-			get_stylesheet_directory_uri() . '/assets/css/style.css',
-			[],
-			$version_string
-		);
-
-		//Enqueue theme stylesheet
-		wp_enqueue_style( 'vela-theme-style' );
 
 		//Inline styles
 		wp_add_inline_style( 'vela_styles', 'vela_get_accent_colors()' );
 	}
 endif;
-add_action( 'wp_enqueue_scripts', 'vela_theme_style' );
+add_action( 'wp_enqueue_scripts', 'vela_theme_styles' );
+
+require get_template_directory() . '/inc/block-patterns.php';
 

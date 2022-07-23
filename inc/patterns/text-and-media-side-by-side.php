a834ea85d222c5daf696311c2106fbc65662ff82
Kris Cochran <74980761+Kcor555@users.noreply.github.com>
2022-07-23 02:04:23 -0400

    text and media cleanup

--- inc/patterns/text-and-media-side-by-side.php
+++ inc/patterns/text-and-media-side-by-side.php
@@ -10,21 +10,25 @@ return array(
 	'content'    => '<! -- wp:group {
 	"backgroundColor":"light-grey","layout":{
 		"type":"default"}} -->
-<div class="wp-block-group has-light-grey-background-color has-background" ><! -- wp:columns -- >
-	<div class="wp-block-columns" ><! -- wp:column -- >
-		<div class="wp-block-column" ><! -- wp:heading {
-	"level":1} -->
+<div class="wp-block-group has-light-grey-background-color has-background" >
+<! -- wp:columns -- >
+	<div class="wp-block-columns" >
+	<! -- wp:column -- >
+		<div class="wp-block-column" >
+		<! -- wp:heading {"level":1} -->
 			<h1 > Profit margins and stuff </h1 >
 			<! -- /wp:heading -- >
 
 			<! -- wp:paragraph -- >
-			<p > Leverage agile frameworks to provide a robust synopsis for high level overviews . Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition . Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment .</p >
-			<! -- /wp:paragraph -- ></div >
+			<p > Leverage agile frameworks to provide a robust synopsis for high level overviews . Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition . Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment .
+			</p >
+			<! -- /wp:paragraph -- >
+			</div >
 		<! -- /wp:column -- >
 
 		<! -- wp:column -- >
-		<div class="wp-block-column" ><! -- wp:image {
-	"id":21,"sizeSlug":"large","linkDestination":"none"} -->
+		<div class="wp-block-column" >
+		<! -- wp:image {"id":21,"sizeSlug":"large","linkDestination":"none"} -->
 			<figure class="wp-block-image size-large" >
 				<img src = "http://test-plugins.lndo.site/wp-content/uploads/2022/07/laptop-g6e383d9d0_1920-1024x640.jpg" 
 				     alt = "" 
